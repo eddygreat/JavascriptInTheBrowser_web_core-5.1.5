68becd3bb371731058c74db6d0a8f6688494ec08
eddygreat <46139760+eddygreat@users.noreply.github.com>
2023-12-30 13:53:57 +0100

    updated

--- javascript.js
+++ javascript.js
@@ -1,11 +1,45 @@
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 10,
+  slidesPerView: 1.2,
+  freeMode: true,
+     
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      enabled: true
+    },
+    breakpoints: {
+      
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 24,
+        //freeMode: true,
+          scrollbar: {
+            el: ".swiper-scrollbar",
+            hide: true,
+                        
+        },
+         
+       
+      },
+      1024: {
+        slidesPerView: 4,
+        spaceBetween: 32,
+        scrollbar: {
+            el: ".swiper-scrollbar",
+            enabled: false,
+                        
+        },
+      
+      },
+    },
+  
+});
+
 let slideIndex = 1;
 
 
-function myFunction(x) {
-    if (x.matches) { 
-        showSlides(slideIndex);
-    }
-}
+
 
 // Create a MediaQueryList object
 var x = window.matchMedia("(min-width: 768px)")
@@ -17,47 +51,11 @@ x.addEventListener("change", function() {
 });
 
 // Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-//switches brands on mobile view
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("brand-units");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) { slideIndex = slides.length }
-  if (x.matches) { // If media query matches
-      for (i = 0; i < slides.length; i++) {
-          slides[i].style.display = "";
-          
-      }
-      for (i = 0; i < dots.length; i++) {
-         dots[i].className = dots[i].className.replace("active", "");
-      }
-      slides[slideIndex-1].style.display = "block";
-      dots[slideIndex-1].className += " active";
-  } else {
-      for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-      }
-      for (i = 0; i < dots.length; i++) {
-         dots[i].className = dots[i].className.replace("active", "");
-      }
-      slides[slideIndex-1].style.display = "block";
-      dots[slideIndex-1].className += " active";
-    
-    } 
-
-   
-
-}
+//function currentSlide(n) {
+//  showSlides(slideIndex = n);
+//}
 
 
-//This block of code controls the changing of the button from
-//show all to hide and vice versa, while implementing the hide and show all
-//function as well.
 const switcher = document.querySelector(".brands-row-three");
 
 document.getElementById('show-button').onclick = function () {
@@ -77,4 +75,7 @@ document.getElementById('show-button').onclick = function () {
    }  
     
 }
+
+
+
 
